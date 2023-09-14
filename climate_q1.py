eeae84f99cef8256dc23141c5fc3f776ef745ae8
GG <gkg.gautam31@gmail.com>
2023-09-14 12:52:39 +1000

    Added sqlite3 and related codes but says table named climate not found

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,27 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
+conn = sqlite3.connect('climate.db')
+cursor = conn.cursor()
+
+query = 'SELECT Year, CO2, Temperature FROM climate'
+cursor.execute(query)
+
+rows = cursor.fetchall()
+
+
 years = []
 co2 = []
 temp = []
+
+
+for row in rows:
+    year, co2_value, temp_value = row[:3]
+    years.append(year)
+    co2.append(co2_value)
+    temp.append(temp_value)
+
+conn.close()
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
@@ -15,4 +34,5 @@ plt.plot(years, temp, 'r*-')
 plt.ylabel("Temp (C)") 
 plt.xlabel("Year (decade)") 
 plt.show() 
-plt.savefig("co2_temp_1.png") 
+plt.savefig("co2_temp_1.png")
+
